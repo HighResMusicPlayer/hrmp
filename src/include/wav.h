2ae229e762fe891b8b20dbd4c486c6d23bd17767
jesperpedersen <jesperpedersen.db@gmail.com>
2025-08-19 00:19:27 -0400

    Align license text

--- src/include/wav.h
+++ src/include/wav.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 HighResMusicPlayer
+ * Copyright (C) 2025 The HighResMusicPlayer community
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
