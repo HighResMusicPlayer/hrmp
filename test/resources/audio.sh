8676dd0f1e67056651d20d4c6a16254cdf179d37
jesperpedersen <jesperpedersen.db@gmail.com>
2025-09-19 08:08:55 -0400

    Add test files

---
+++ test/resources/audio.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Makes use of swavgen
+# https://github.com/ymich9963/swavgen
+# v1.1.4+
+
+rm -f *.wav
+rm -f *.flac
+
+./swavgen sine -s 44100 -e PCM -d 1 -l 16 -c 2 -o 44100-PCM-16-2.wav
+./swavgen sine -s 48000 -e PCM -d 1 -l 16 -c 2 -o 48000-PCM-16-2.wav
+./swavgen sine -s 88200 -e PCM -d 1 -l 16 -c 2 -o 88200-PCM-16-2.wav
+./swavgen sine -s 96000 -e PCM -d 1 -l 16 -c 2 -o 96000-PCM-16-2.wav
+./swavgen sine -s 176400 -e PCM -d 1 -l 16 -c 2 -o 176400-PCM-16-2.wav
+./swavgen sine -s 192000 -e PCM -d 1 -l 16 -c 2 -o 192000-PCM-16-2.wav
+./swavgen sine -s 352800 -e PCM -d 1 -l 16 -c 2 -o 352800-PCM-16-2.wav
+./swavgen sine -s 384000 -e PCM -d 1 -l 16 -c 2 -o 384000-PCM-16-2.wav
+./swavgen sine -s 705600 -e PCM -d 1 -l 16 -c 2 -o 705600-PCM-16-2.wav
+./swavgen sine -s 768000 -e PCM -d 1 -l 16 -c 2 -o 768000-PCM-16-2.wav
+
+./swavgen sine -s 44100 -e PCM -d 1 -l 24 -c 2 -o 44100-PCM-24-2.wav
+./swavgen sine -s 48000 -e PCM -d 1 -l 24 -c 2 -o 48000-PCM-24-2.wav
+./swavgen sine -s 88200 -e PCM -d 1 -l 24 -c 2 -o 88200-PCM-24-2.wav
+./swavgen sine -s 96000 -e PCM -d 1 -l 24 -c 2 -o 96000-PCM-24-2.wav
+./swavgen sine -s 176400 -e PCM -d 1 -l 24 -c 2 -o 176400-PCM-24-2.wav
+./swavgen sine -s 192000 -e PCM -d 1 -l 24 -c 2 -o 192000-PCM-24-2.wav
+./swavgen sine -s 352800 -e PCM -d 1 -l 24 -c 2 -o 352800-PCM-24-2.wav
+./swavgen sine -s 384000 -e PCM -d 1 -l 24 -c 2 -o 384000-PCM-24-2.wav
+./swavgen sine -s 705600 -e PCM -d 1 -l 24 -c 2 -o 705600-PCM-24-2.wav
+./swavgen sine -s 768000 -e PCM -d 1 -l 24 -c 2 -o 768000-PCM-24-2.wav
+
+./swavgen sine -s 44100 -e PCM -d 1 -l 32 -c 2 -o 44100-PCM-32-2.wav
+./swavgen sine -s 48000 -e PCM -d 1 -l 32 -c 2 -o 48000-PCM-32-2.wav
+./swavgen sine -s 88200 -e PCM -d 1 -l 32 -c 2 -o 88200-PCM-32-2.wav
+./swavgen sine -s 96000 -e PCM -d 1 -l 32 -c 2 -o 96000-PCM-32-2.wav
+./swavgen sine -s 176400 -e PCM -d 1 -l 32 -c 2 -o 176400-PCM-32-2.wav
+./swavgen sine -s 192000 -e PCM -d 1 -l 32 -c 2 -o 192000-PCM-32-2.wav
+./swavgen sine -s 352800 -e PCM -d 1 -l 32 -c 2 -o 352800-PCM-32-2.wav
+./swavgen sine -s 384000 -e PCM -d 1 -l 32 -c 2 -o 384000-PCM-32-2.wav
+./swavgen sine -s 705600 -e PCM -d 1 -l 32 -c 2 -o 705600-PCM-32-2.wav
+./swavgen sine -s 768000 -e PCM -d 1 -l 32 -c 2 -o 768000-PCM-32-2.wav
+
+ffmpeg -i 176400-PCM-16-2.wav -c:a flac -sample_fmt s16 176400-PCM-16-2.flac
+ffmpeg -i 192000-PCM-16-2.wav -c:a flac -sample_fmt s16 192000-PCM-16-2.flac
+ffmpeg -i 352800-PCM-16-2.wav -c:a flac -sample_fmt s16 352800-PCM-16-2.flac
+ffmpeg -i 384000-PCM-16-2.wav -c:a flac -sample_fmt s16 384000-PCM-16-2.flac
+ffmpeg -i 44100-PCM-16-2.wav -c:a flac -sample_fmt s16 44100-PCM-16-2.flac
+ffmpeg -i 48000-PCM-16-2.wav -c:a flac -sample_fmt s16 48000-PCM-16-2.flac
+ffmpeg -i 88200-PCM-16-2.wav -c:a flac -sample_fmt s16 88200-PCM-16-2.flac
+ffmpeg -i 96000-PCM-16-2.wav -c:a flac -sample_fmt s16 96000-PCM-16-2.flac
+
+ffmpeg -i 176400-PCM-24-2.wav -c:a flac -sample_fmt s32 176400-PCM-24-2.flac
+ffmpeg -i 192000-PCM-24-2.wav -c:a flac -sample_fmt s32 192000-PCM-24-2.flac
+ffmpeg -i 352800-PCM-24-2.wav -c:a flac -sample_fmt s32 352800-PCM-24-2.flac
+ffmpeg -i 384000-PCM-24-2.wav -c:a flac -sample_fmt s32 384000-PCM-24-2.flac
+ffmpeg -i 44100-PCM-24-2.wav -c:a flac -sample_fmt s32 44100-PCM-24-2.flac
+ffmpeg -i 48000-PCM-24-2.wav -c:a flac -sample_fmt s32 48000-PCM-24-2.flac
+ffmpeg -i 88200-PCM-24-2.wav -c:a flac -sample_fmt s32 88200-PCM-24-2.flac
+ffmpeg -i 96000-PCM-24-2.wav -c:a flac -sample_fmt s32 96000-PCM-24-2.flac
+
+ffmpeg -i 176400-PCM-32-2.wav -c:a flac -sample_fmt s32 -strict experimental 176400-PCM-32-2.flac
+ffmpeg -i 192000-PCM-32-2.wav -c:a flac -sample_fmt s32 -strict experimental 192000-PCM-32-2.flac
+ffmpeg -i 352800-PCM-32-2.wav -c:a flac -sample_fmt s32 -strict experimental 352800-PCM-32-2.flac
+ffmpeg -i 384000-PCM-32-2.wav -c:a flac -sample_fmt s32 -strict experimental 384000-PCM-32-2.flac
+ffmpeg -i 44100-PCM-32-2.wav -c:a flac -sample_fmt s32 -strict experimental 44100-PCM-32-2.flac
+ffmpeg -i 48000-PCM-32-2.wav -c:a flac -sample_fmt s32 -strict experimental 48000-PCM-32-2.flac
+ffmpeg -i 88200-PCM-32-2.wav -c:a flac -sample_fmt s32 -strict experimental 88200-PCM-32-2.flac
+ffmpeg -i 96000-PCM-32-2.wav -c:a flac -sample_fmt s32 -strict experimental 96000-PCM-32-2.flac
