61fe4d0ce34f761d2af572124cfca743899d4807
jesperpedersen <jesperpedersen.db@gmail.com>
2025-09-22 18:43:35 -0400

    Add quiet

--- test/resources/audio.sh
+++ test/resources/audio.sh
@@ -2,43 +2,43 @@
 
 # Makes use of swavgen
 # https://github.com/ymich9963/swavgen
-# v1.1.4+
+# v1.1.5+
 
 rm -f *.wav
 rm -f *.flac
 
-./swavgen sine -s 44100 -e PCM -d 1 -l 16 -c 2 -o 44100-PCM-16-2.wav
-./swavgen sine -s 48000 -e PCM -d 1 -l 16 -c 2 -o 48000-PCM-16-2.wav
-./swavgen sine -s 88200 -e PCM -d 1 -l 16 -c 2 -o 88200-PCM-16-2.wav
-./swavgen sine -s 96000 -e PCM -d 1 -l 16 -c 2 -o 96000-PCM-16-2.wav
-./swavgen sine -s 176400 -e PCM -d 1 -l 16 -c 2 -o 176400-PCM-16-2.wav
-./swavgen sine -s 192000 -e PCM -d 1 -l 16 -c 2 -o 192000-PCM-16-2.wav
-./swavgen sine -s 352800 -e PCM -d 1 -l 16 -c 2 -o 352800-PCM-16-2.wav
-./swavgen sine -s 384000 -e PCM -d 1 -l 16 -c 2 -o 384000-PCM-16-2.wav
-./swavgen sine -s 705600 -e PCM -d 1 -l 16 -c 2 -o 705600-PCM-16-2.wav
-./swavgen sine -s 768000 -e PCM -d 1 -l 16 -c 2 -o 768000-PCM-16-2.wav
+./swavgen sine -s 44100 -q -e PCM -d 1 -l 16 -c 2 -o 44100-PCM-16-2.wav
+./swavgen sine -s 48000 -q -e PCM -d 1 -l 16 -c 2 -o 48000-PCM-16-2.wav
+./swavgen sine -s 88200 -q -e PCM -d 1 -l 16 -c 2 -o 88200-PCM-16-2.wav
+./swavgen sine -s 96000 -q -e PCM -d 1 -l 16 -c 2 -o 96000-PCM-16-2.wav
+./swavgen sine -s 176400 -q -e PCM -d 1 -l 16 -c 2 -o 176400-PCM-16-2.wav
+./swavgen sine -s 192000 -q -e PCM -d 1 -l 16 -c 2 -o 192000-PCM-16-2.wav
+./swavgen sine -s 352800 -q -e PCM -d 1 -l 16 -c 2 -o 352800-PCM-16-2.wav
+./swavgen sine -s 384000 -q -e PCM -d 1 -l 16 -c 2 -o 384000-PCM-16-2.wav
+./swavgen sine -s 705600 -q -e PCM -d 1 -l 16 -c 2 -o 705600-PCM-16-2.wav
+./swavgen sine -s 768000 -q -e PCM -d 1 -l 16 -c 2 -o 768000-PCM-16-2.wav
 
-./swavgen sine -s 44100 -e PCM -d 1 -l 24 -c 2 -o 44100-PCM-24-2.wav
-./swavgen sine -s 48000 -e PCM -d 1 -l 24 -c 2 -o 48000-PCM-24-2.wav
-./swavgen sine -s 88200 -e PCM -d 1 -l 24 -c 2 -o 88200-PCM-24-2.wav
-./swavgen sine -s 96000 -e PCM -d 1 -l 24 -c 2 -o 96000-PCM-24-2.wav
-./swavgen sine -s 176400 -e PCM -d 1 -l 24 -c 2 -o 176400-PCM-24-2.wav
-./swavgen sine -s 192000 -e PCM -d 1 -l 24 -c 2 -o 192000-PCM-24-2.wav
-./swavgen sine -s 352800 -e PCM -d 1 -l 24 -c 2 -o 352800-PCM-24-2.wav
-./swavgen sine -s 384000 -e PCM -d 1 -l 24 -c 2 -o 384000-PCM-24-2.wav
-./swavgen sine -s 705600 -e PCM -d 1 -l 24 -c 2 -o 705600-PCM-24-2.wav
-./swavgen sine -s 768000 -e PCM -d 1 -l 24 -c 2 -o 768000-PCM-24-2.wav
+./swavgen sine -s 44100 -q -e PCM -d 1 -l 24 -c 2 -o 44100-PCM-24-2.wav
+./swavgen sine -s 48000 -q -e PCM -d 1 -l 24 -c 2 -o 48000-PCM-24-2.wav
+./swavgen sine -s 88200 -q -e PCM -d 1 -l 24 -c 2 -o 88200-PCM-24-2.wav
+./swavgen sine -s 96000 -q -e PCM -d 1 -l 24 -c 2 -o 96000-PCM-24-2.wav
+./swavgen sine -s 176400 -q -e PCM -d 1 -l 24 -c 2 -o 176400-PCM-24-2.wav
+./swavgen sine -s 192000 -q -e PCM -d 1 -l 24 -c 2 -o 192000-PCM-24-2.wav
+./swavgen sine -s 352800 -q -e PCM -d 1 -l 24 -c 2 -o 352800-PCM-24-2.wav
+./swavgen sine -s 384000 -q -e PCM -d 1 -l 24 -c 2 -o 384000-PCM-24-2.wav
+./swavgen sine -s 705600 -q -e PCM -d 1 -l 24 -c 2 -o 705600-PCM-24-2.wav
+./swavgen sine -s 768000 -q -e PCM -d 1 -l 24 -c 2 -o 768000-PCM-24-2.wav
 
-./swavgen sine -s 44100 -e PCM -d 1 -l 32 -c 2 -o 44100-PCM-32-2.wav
-./swavgen sine -s 48000 -e PCM -d 1 -l 32 -c 2 -o 48000-PCM-32-2.wav
-./swavgen sine -s 88200 -e PCM -d 1 -l 32 -c 2 -o 88200-PCM-32-2.wav
-./swavgen sine -s 96000 -e PCM -d 1 -l 32 -c 2 -o 96000-PCM-32-2.wav
-./swavgen sine -s 176400 -e PCM -d 1 -l 32 -c 2 -o 176400-PCM-32-2.wav
-./swavgen sine -s 192000 -e PCM -d 1 -l 32 -c 2 -o 192000-PCM-32-2.wav
-./swavgen sine -s 352800 -e PCM -d 1 -l 32 -c 2 -o 352800-PCM-32-2.wav
-./swavgen sine -s 384000 -e PCM -d 1 -l 32 -c 2 -o 384000-PCM-32-2.wav
-./swavgen sine -s 705600 -e PCM -d 1 -l 32 -c 2 -o 705600-PCM-32-2.wav
-./swavgen sine -s 768000 -e PCM -d 1 -l 32 -c 2 -o 768000-PCM-32-2.wav
+./swavgen sine -s 44100 -q -e PCM -d 1 -l 32 -c 2 -o 44100-PCM-32-2.wav
+./swavgen sine -s 48000 -q -e PCM -d 1 -l 32 -c 2 -o 48000-PCM-32-2.wav
+./swavgen sine -s 88200 -q -e PCM -d 1 -l 32 -c 2 -o 88200-PCM-32-2.wav
+./swavgen sine -s 96000 -q -e PCM -d 1 -l 32 -c 2 -o 96000-PCM-32-2.wav
+./swavgen sine -s 176400 -q -e PCM -d 1 -l 32 -c 2 -o 176400-PCM-32-2.wav
+./swavgen sine -s 192000 -q -e PCM -d 1 -l 32 -c 2 -o 192000-PCM-32-2.wav
+./swavgen sine -s 352800 -q -e PCM -d 1 -l 32 -c 2 -o 352800-PCM-32-2.wav
+./swavgen sine -s 384000 -q -e PCM -d 1 -l 32 -c 2 -o 384000-PCM-32-2.wav
+./swavgen sine -s 705600 -q -e PCM -d 1 -l 32 -c 2 -o 705600-PCM-32-2.wav
+./swavgen sine -s 768000 -q -e PCM -d 1 -l 32 -c 2 -o 768000-PCM-32-2.wav
 
 ffmpeg -i 176400-PCM-16-2.wav -c:a flac -sample_fmt s16 176400-PCM-16-2.flac
 ffmpeg -i 192000-PCM-16-2.wav -c:a flac -sample_fmt s16 192000-PCM-16-2.flac
