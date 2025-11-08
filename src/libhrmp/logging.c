36dc22aacc2b15c10ca8a12a0d964df1ea097e89
jesperpedersen <jesperpedersen.db@gmail.com>
2025-11-07 20:44:20 -0500

    Improve hrmp_log_mem

--- src/libhrmp/logging.c
+++ src/libhrmp/logging.c
@@ -407,6 +407,7 @@ retry:
             {
                size_t offset = 0;
                size_t remaining = size;
+               bool full_line = false;
 
                while (remaining > 0)
                {
@@ -432,6 +433,22 @@ retry:
                      else
                      {
                         n = hrmp_append_char(n, '?');
+                     }
+                  }
+
+                  if (strlen(l) == LINE_LENGTH * 2)
+                  {
+                     full_line = true;
+                  }
+                  else if (full_line)
+                  {
+                     if (strlen(l) < LINE_LENGTH * 2)
+                     {
+                        int chars_missing = (LINE_LENGTH * 2) - strlen(l);
+                        for (int i = 0; i < chars_missing; i++)
+                        {
+                           l = hrmp_append_char(l, ' ');
+                        }
                      }
                   }
 
